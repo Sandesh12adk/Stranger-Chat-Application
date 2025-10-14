db41a5e6112b7ab43728e7d389b4eb528e2fc54a
ACER <sandesh12adk@gmil.com>
2025-10-15 00:47:57 +0545

    Successfully deployed need to change to pp, make chat ui responseive and chanter @Creater to @Creator

---
+++ gatewayserver/src/main/java/com/example/gatewayserver/config/myCorsWebFilter.java
@@ -0,0 +1,29 @@
+package com.example.gatewayserver.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+
+@Configuration
+public class myCorsWebFilter {
+
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOrigins(Arrays.asList("http://156.67.31.159:1111"));
+        config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        config.setAllowCredentials(true);
+        config.addAllowedHeader("*"); // allow all headers
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsWebFilter(source);
+    }
+}
+
+
